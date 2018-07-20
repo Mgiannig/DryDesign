f81bf1cb1dd715e9bce57340e68e8a43378fb55c
Mariano Gianni <mariano_gianni@hotmail.com>
2018-07-20 18:56:40 -0300

    Added Javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+ 
+  
     
   });
+
+  
+
+
+function addElementJsBox() {
+
+  var newLi = document.createElement("li");
+  var newA = document.createElement("a");
+
+  let boxes = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+
+  boxes.appendChild(newLi);
+  newLi.appendChild(newA)
+
+  newA.innerHTML = "added!";
+
+}
+
